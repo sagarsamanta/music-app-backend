9e08ddfa95734d088db6f089a3540b7b7c4e1dbd
Sagar Samanta <sagarsamanta390@gmail.com>
2022-10-12 19:00:40 +0530

    work on doc upload and downlod

--- controllers/document.js
+++ controllers/document.js
@@ -1,17 +1,54 @@
+const { monthlyStoreDocUpload } = require("../middleware/Upload");
 const Doc = require("../models/Document");
-const data = {
-  storeName: "ganna",
-  year: "2022",
-  month: "march",
-  userName: "kk",
-  revanue: 30,
-  total: 20,
+const fs = require("fs");
+const objectsToCsv = require("objects-to-csv");
+const excelToJson = require("convert-excel-to-json");
+const XLSX = require("xlsx");
+const getRecords = async (query) => {
+  const data = await Doc.find(query);
+  console.log(data);
+  return data;
 };
+const jsonToCsv = async (req, res, data, store, month, year) => {
+  const workSheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new;
+  XLSX.utils.book_append_sheet(workbook, workSheet, "students");
+  XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+  XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
+  XLSX.writeFile(workbook, "studentData.xlsx");
+};
+
 exports.addDocument = async (req, res, next) => {
-  const newDoc = new Doc(data);
-  const doc = await newDoc.save();
-  res.status(200).send(doc);
   try {
+    monthlyStoreDocUpload(req, res, function (err) {
+      const { storeName, month, year } = req.body;
+      const result = excelToJson({
+        sourceFile: `public/${req.file.filename}`,
+        columnToKey: {
+          "*": "{{columnHeader}}",
+        },
+        header: {
+          rows: 1,
+        },
+      });
+      if (result.Sheet1.length > 0) {
+        const updatedRecord = result.Sheet1.map((record) => {
+          //   const fiterArtistName=record.artist_name.split(",")[0]
+          return { storeName, month, year, ...record };
+        });
+
+        Doc.insertMany(updatedRecord, (err, data) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).send("Failed to upload!");
+          }
+        });
+        fs.unlinkSync(`public/${req.file.filename}`);
+        return res.status(200).json({ message: "Successfully uploaded!" });
+      } else {
+        return res.status(500).send(err);
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
@@ -23,7 +60,7 @@ exports.getMonthStoreReport = async (req, res) => {
     const monthReport = await Doc.aggregate([
       {
         $match: {
-          userName: req.body.userName,
+          artist_name: req.body.artist_name,
         },
       },
       {
@@ -33,7 +70,7 @@ exports.getMonthStoreReport = async (req, res) => {
             month: "$month",
             // storeName: "$storeName",
           },
-          revanue: { $sum: "$revanue" },
+          revanue: { $sum: "$income" },
           streamming: { $sum: "$total" },
         },
       },
@@ -41,7 +78,7 @@ exports.getMonthStoreReport = async (req, res) => {
     const storeReport = await Doc.aggregate([
       {
         $match: {
-          userName: req.body.userName,
+          artist_name: req.body.artist_name,
         },
       },
       {
@@ -51,7 +88,7 @@ exports.getMonthStoreReport = async (req, res) => {
             // month: "$month",
             storeName: "$storeName",
           },
-          revanue: { $sum: "$revanue" },
+          revanue: { $sum: "$income" },
           streamming: { $sum: "$total" },
         },
       },
@@ -70,7 +107,7 @@ exports.getStreamingReport = async (req, res) => {
     const report = await Doc.aggregate([
       {
         $match: {
-          userName: req.body.userName,
+          artist_name: req.body.artist_name,
         },
       },
       {
@@ -98,7 +135,7 @@ exports.getRevanueReport = async (req, res) => {
     const report = await Doc.aggregate([
       {
         $match: {
-          userName: req.body.userName,
+          artist_name: req.body.artist_name,
         },
       },
       {
@@ -108,7 +145,7 @@ exports.getRevanueReport = async (req, res) => {
             month: "$month",
             storeName: "$storeName",
           },
-          revanue: { $sum: "$revanue" },
+          revanue: { $sum: "$income" },
           //   streamming: { $sum: "$total" },
         },
       },
@@ -117,6 +154,37 @@ exports.getRevanueReport = async (req, res) => {
     res.status(200).send({
       report,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+exports.removeUploadedExcel = async (req, res) => {
+  try {
+    const { storeName, year, month } = req.params;
+    const count = await Doc.deleteMany({ storeName, year, month });
+    res.status(200).send(count);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+exports.getUserReports = async (req, res) => {
+  try {
+    const { artist_name, storeName, month, year } = req.body;
+    if (storeName && month && year) {
+      const data = await getRecords({ artist_name, storeName, month, year });
+      await jsonToCsv(req, res, data, storeName, year, month);
+      //   return res.status(200).send(data);
+    } else if (storeName && year) {
+      const data = await getRecords({ artist_name, storeName, year });
+      return res.status(200).send(data);
+    } else if (storeName) {
+      const data = await getRecords({ artist_name, storeName });
+      return res.status(200).send(data);
+    } else if (year) {
+      const data = await getRecords({ artist_name, year });
+      return res.status(200).send(data);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
