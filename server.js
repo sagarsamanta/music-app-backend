c445983305b28fd4babebdf03abe3184e7ebd016
Sagar Samanta <sagarsamanta390@gmail.com>
2022-12-31 12:54:37 +0530

    remove method from cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(
   cors({
     origin: "https://mtool.bengalpictures.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    // methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(express.static(__dirname + "/public"));
